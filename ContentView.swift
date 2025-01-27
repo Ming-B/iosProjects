f12bc73fcc8b3c3c6c1f861688cc349864277ae3
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-01-27 09:26:37 -0500

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -18,31 +18,61 @@ struct Message : Identifiable {
         self.date = Date()
     }
     
-    func getDateFormat(date:Date) -> String {
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateStyle = .short
-        dateFormatter.timeStyle = .medium
-        
-        dateFormatter.locale = Locale(identifier: "en_US")
-        
-        
-        let newFormattedDate = dateFormatter.string(from: date)
-        
-        return newFormattedDate
-        
-        
-    }
+}
+
+func getDateFormat(date:Date) -> String {
+    let dateFormatter = DateFormatter()
+    
+    dateFormatter.dateStyle = .short
+    dateFormatter.timeStyle = .medium
+    
+    dateFormatter.locale = Locale(identifier: "en_US")
+    
+    
+    let newFormattedDate = dateFormatter.string(from: date)
+    
+    return newFormattedDate
+    
+    
 }
 
 
 struct ContentView: View {
+    @State private var messages: [Message] = [
+        Message(message: "hello?"),
+        Message(message: "is this thing on?"),
+        Message(message: "I would like to create an app that is useful for people"),
+        Message(message: "Maybe an app where people on campus can chat with each other?")
+    ]
+    
+    @State private var newMessage:String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List{
+                
+            }
+
+        }
+        .padding()
+    }
+}
+
+struct MessageView: View {
+    var message : Message
+    
+    var body: some View {
+        VStack{
+            Text(message.message)
+                .padding()
+                .background(Color.blue.opacity(0.1))
+                .cornerRadius(30.0)
+                .foregroundStyle(.white)
+            
+            Text(getDateFormat(date: message.date))
+                .font(.footnote)
+                .foregroundStyle(.gray)
+            
         }
         .padding()
     }
